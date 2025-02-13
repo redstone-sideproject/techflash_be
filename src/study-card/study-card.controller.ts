4e9709fcf193bde9a6da5ebe1962ef4a18effad8
k-redstone <che0618@naver.com>
2025-02-13 16:21:56 +0900

    feat: @nestjs/axios 설정

--- src/study-card/study-card.controller.ts
+++ src/study-card/study-card.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common'
+import { StudyCardService } from '@src/study-card/study-card.service'
 
 @Controller('study-card')
-export class StudyCardController {}
+export class StudyCardController {
+  constructor(private readonly studyCardService: StudyCardService) {}
+}
--- src/study-card/study-card.service.ts
+++ src/study-card/study-card.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common'
 
+import { HttpService } from '@nestjs/axios'
+import { ConfigService } from '@nestjs/config'
 @Injectable()
-export class StudyCardService {}
+export class StudyCardService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
+}
